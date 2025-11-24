4bd6f2b9786bcaac00a034a1883d2adcbc3fd512
Salman-Hussain21 <isalmanhussain21@gmail.com>
2025-11-25 02:56:53 +0500

    session2 basics

---
+++ session2 - Basics/basics.py
@@ -0,0 +1,37 @@
+# #datatypes
+# print('hello world') #string
+# print(1234) #integer
+# print(-100) #negative integer
+# print(12.34) #float
+# print(-10.5) #negative float
+# print(True) #boolean
+# print(False) #boolean  
+
+# #variables
+# name = 'salman'
+# age = 19
+# is_student = True
+# print(name)
+# print(age)
+# print(is_student)
+
+# #operations
+# a = 10
+# b = 5
+# print(a + b)  # addition
+# print(a - b)  # subtraction
+# print(a * b)  # multiplication
+# print(a / b)  # division
+# print(a // b) # floor division
+# print(a % b)  # modulus
+# print(a ** b) # exponentiation
+# print(a + 2 * b) # operator precedence
+# print((a + 2) * b) # parentheses to change precedence
+
+# #input
+# name = input('Enter your name: ')
+# print(name)
+
+#assignment input
+name = input('Please Enter Your Name:  ')
+print('Hello', name, ' have a nice day!')
